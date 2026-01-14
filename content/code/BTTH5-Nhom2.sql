ccce8694c62587235b2f3b8684418cce965541a5
sam-uit <25410291@ms.uit.edu.vn>
2026-01-14 20:34:05 +0800

    Update BTTH5-Nhom2.sql

--- content/code/BTTH5-Nhom2.sql
+++ content/code/BTTH5-Nhom2.sql
@@ -245,6 +245,29 @@ GO
 
 -- Câu 10.
 
+-- Câu 10. Xquery kiểm tra xem có tồn tại sinh viên tên ‘Lan’ trong trường CNTT
+SELECT ChiTietSV.exist('/THONGTINSV/sinhvien[@Ten="Lan"]') as KetQua
+FROM QuanLySV WHERE TenDH = 'DH CNTT'
+GO
+
+-- Câu 10b. INSERT dữ liệu mẫu vào THONGTINSV
+UPDATE QuanLySV
+SET ChiTietSV.modify('
+  insert <sinhvien ID="15" Ten="Lan">
+            <monhoc ID="10" Ten="Toan Roi Rac" />
+            <monhoc ID="11" Ten="Lap Trinh C#" />
+            <monhoc ID="12" Ten="CSDL Nang Cao" />
+         </sinhvien>
+  as last into (/THONGTINSV)[1]
+')
+WHERE TenDH = 'DH CNTT'
+GO
+
+-- Câu 10d. Xquery kiểm tra xem có tồn tại sinh viên tên ‘Lan’ trong trường CNTT
+SELECT ChiTietSV.query('/THONGTINSV/sinhvien[@Ten="Lan"]') as KetQua
+FROM QuanLySV WHERE TenDH = 'DH CNTT'
+GO
+
 
 -- Câu 11.
 
