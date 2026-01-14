6cb5b666798fe83bfd6d4736176e75c6cc912abd
sam-uit <25410291@ms.uit.edu.vn>
2026-01-14 17:33:43 +0800

    Update BTTH5-a-insert-MonHoc.sql

---
+++ content/code/BTTH5-a-insert-MonHoc.sql
@@ -0,0 +1,11 @@
+-- Insert MonHoc
+INSERT INTO MonHoc (TenMonHoc) SELECT ('Co So Du Lieu')
+INSERT INTO MonHoc (TenMonHoc) SELECT ('Cau Truc Du Lieu')
+INSERT INTO MonHoc (TenMonHoc) SELECT ('Lap Trinh Di Dong')
+INSERT INTO MonHoc (TenMonHoc) SELECT ('Toan Giai Tich')
+INSERT INTO MonHoc (TenMonHoc) SELECT ('Lap Trinh Java')
+INSERT INTO MonHoc (TenMonHoc) SELECT ('He Quan Tri CSDL')
+INSERT INTO MonHoc (TenMonHoc) SELECT ('Anh Van')
+INSERT INTO MonHoc (TenMonHoc) SELECT ('Thiet Ke Web ')
+INSERT INTO MonHoc (TenMonHoc) SELECT ('An Toan Thong Tin')
+GO
