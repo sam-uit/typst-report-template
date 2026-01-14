add8a94a9723ce0f8567c388e44ef7333de6804c
sam-uit <25410291@ms.uit.edu.vn>
2026-01-14 20:32:02 +0800

    Update BTTH5-Nhom2.sql

--- content/code/BTTH5-Nhom2.sql
+++ content/code/BTTH5-Nhom2.sql
@@ -204,6 +204,14 @@ GO
 
 -- Câu 7.
 
+-- Câu 7. Thông tin sinh viên có tên Nam hoặc Thanh
+SELECT ChiTietSV.query('
+  for $s in /THONGTINSV/sinhvien
+  where $s/@Ten = "Nam" or $s/@Ten = "Thanh"
+  return $s
+') as KetQua
+FROM QuanLySV
+GO
 
 -- Câu 8.
 
