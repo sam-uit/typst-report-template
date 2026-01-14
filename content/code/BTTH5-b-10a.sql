62c2eea7c0bfc5a895ebc2cab4f80964c6ea8788
sam-uit <25410291@ms.uit.edu.vn>
2026-01-14 18:35:45 +0800

    Update BTTH5-b-10a.sql

--- content/code/BTTH5-b-10a.sql
+++ content/code/BTTH5-b-10a.sql
@@ -1,4 +1,4 @@
--- Xquery kiểm tra xem có tồn tại sinh viên tên ‘Lan’ trong trường CNTT
+-- Câu 10. Xquery kiểm tra xem có tồn tại sinh viên tên ‘Lan’ trong trường CNTT
 SELECT ChiTietSV.exist('/THONGTINSV/sinhvien[@Ten="Lan"]') as KetQua
 FROM QuanLySV WHERE TenDH = 'DH CNTT'
 GO
