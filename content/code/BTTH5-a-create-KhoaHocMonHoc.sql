ebe8225ba851558473562452027864e217270189
sam-uit <25410291@ms.uit.edu.vn>
2026-01-14 17:31:12 +0800

    Update BTTH5-a-create-KhoaHocMonHoc.sql

---
+++ content/code/BTTH5-a-create-KhoaHocMonHoc.sql
@@ -0,0 +1,9 @@
+-- KhoaHocMonHoc
+CREATE TABLE KhoaHocMonHoc
+(
+    MaKhoaHoc INT CONSTRAINT FK_KhoaHocMonHoc_MaKhoaHoc FOREIGN KEY REFERENCES
+    KhoaHoc(MaKhoaHoc),
+    MaMonHoc INT CONSTRAINT FK_KhoaHocMonHoc_MaMonHoc FOREIGN KEY REFERENCES
+    MonHoc(MaMonHoc)
+)
+GO
