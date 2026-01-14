4b90f7fda66828cf180a8641ad3e698c748aefac
sam-uit <25410291@ms.uit.edu.vn>
2026-01-14 20:32:29 +0800

    Update BTTH5-Nhom2.sql

--- content/code/BTTH5-Nhom2.sql
+++ content/code/BTTH5-Nhom2.sql
@@ -221,6 +221,12 @@ SET ChiTietSV.modify('replace value of (/THONGTINSV/sinhvien)[2]/@Ten with "Binh
 WHERE TenDH = 'DH CNTT'
 GO
 
+-- Câu 8. Kiểm tra kết quả, tên sinh viên thứ 2 đổi thành 'Binh'
+SELECT ChiTietSV.query('/THONGTINSV/sinhvien') AS KetQua
+FROM QuanLySV
+WHERE TenDH = 'DH CNTT'
+GO
+
 -- Câu 9.
 
 
