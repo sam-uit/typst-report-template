afa6f3eb09b1748c8d1e175b865514f8430450b5
sam-uit <25410291@ms.uit.edu.vn>
2026-01-14 18:25:28 +0800

    Update BTTH5-b-9.sql

--- content/code/BTTH5-b-9.sql
+++ content/code/BTTH5-b-9.sql
@@ -1,3 +1,4 @@
+-- Câu 9. Kiểm tra xem có tồn tại sinh viên có ID là 12 trong trường KHTN không?
 SELECT ChiTietSV.exist('/THONGTINSV/sinhvien[@ID="12"]') as KetQua
 FROM QuanLySV WHERE TenDH = 'DH KHTN'
 GO
