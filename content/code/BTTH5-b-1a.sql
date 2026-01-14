54a79dd0ad2ac2f18d042a635515b36c18d578af
sam-uit <25410291@ms.uit.edu.vn>
2026-01-14 20:39:10 +0800

    Update BTTH5-b-1a.sql

--- content/code/BTTH5-b-1a.sql
+++ content/code/BTTH5-b-1a.sql
@@ -1,4 +1,4 @@
--- Xpath lấy Sinh viên có ID=10
+-- Câu 1. Xpath lấy Sinh viên có ID=10
 SELECT ChiTietSV.query('/THONGTINSV/sinhvien[@ID="10"]') AS KetQua
 FROM QuanLySV WHERE TenDH = 'DH CNTT'
 GO
