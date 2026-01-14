ca0d73fdf8acc6ce5c272e48a886cc0089d5079f
sam-uit <25410291@ms.uit.edu.vn>
2026-01-14 17:33:41 +0800

    Update BTTH5-a-insert-KhoaHocMonHoc.sql

---
+++ content/code/BTTH5-a-insert-KhoaHocMonHoc.sql
@@ -0,0 +1,11 @@
+-- Insert KhoaHocMonHoc
+INSERT INTO KhoaHocMonHoc (MaKhoaHoc,MaMonHoc) SELECT 1,1
+INSERT INTO KhoaHocMonHoc (MaKhoaHoc,MaMonHoc) SELECT 1,2
+INSERT INTO KhoaHocMonHoc (MaKhoaHoc,MaMonHoc) SELECT 1,3
+INSERT INTO KhoaHocMonHoc (MaKhoaHoc,MaMonHoc) SELECT 2,4
+INSERT INTO KhoaHocMonHoc (MaKhoaHoc,MaMonHoc) SELECT 2,5
+INSERT INTO KhoaHocMonHoc (MaKhoaHoc,MaMonHoc) SELECT 2,6
+INSERT INTO KhoaHocMonHoc (MaKhoaHoc,MaMonHoc) SELECT 3,7
+INSERT INTO KhoaHocMonHoc (MaKhoaHoc,MaMonHoc) SELECT 3,8
+INSERT INTO KhoaHocMonHoc (MaKhoaHoc,MaMonHoc) SELECT 3,9
+GO
