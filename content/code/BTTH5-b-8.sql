af1f10b01e46e4912908a062447e009fcdc7d2e8
sam-uit <25410291@ms.uit.edu.vn>
2026-01-14 18:17:42 +0800

    Update BTTH5-b-8.sql

--- content/code/BTTH5-b-8.sql
+++ content/code/BTTH5-b-8.sql
@@ -1,3 +1,4 @@
+-- Câu 8. Thay đổi tên sinh viên thứ 2 thành tên 'Binh' trong trường CNTT
 UPDATE QuanLySV
 SET ChiTietSV.modify('replace value of [/THONGTINSV/sinhvien](2)/@Ten with "Binh"')
 WHERE TenDH = 'DH CNTT'
