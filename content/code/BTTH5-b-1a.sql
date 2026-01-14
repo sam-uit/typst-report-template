3e2d7a48e667ff7b9bbac4192b5af4a5ac3a741d
sam-uit <25410291@ms.uit.edu.vn>
2026-01-14 17:06:55 +0800

    code: 1a, 1b.

---
+++ content/code/BTTH5-b-1a.sql
@@ -0,0 +1,4 @@
+-- Xpath lấy Sinh viên có ID=10
+SELECT ChiTietSV.query('/THONGTINSV/sinhvien[@ID="10"]') AS KetQua
+FROM QuanLySV WHERE TenDH = 'DH CNTT'
+GO
--- content/code/BTTH5-b-1b.sql
+++ content/code/BTTH5-b-1b.sql
@@ -1,8 +1,3 @@
--- Xpath lấy Sinh viên có ID=10
-SELECT ChiTietSV.query('/THONGTINSV/sinhvien[@ID="10"]') AS KetQua
-FROM QuanLySV WHERE TenDH = 'DH CNTT'
-GO
-
 -- Xpath lấy sinh viên ở vị trí cuối cùng ở trường CNTT
 SELECT ChiTietSV.query('/THONGTINSV/sinhvien[last()]') AS KetQua
 FROM QuanLySV WHERE TenDH = 'DH CNTT'
