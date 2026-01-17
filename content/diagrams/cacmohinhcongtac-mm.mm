9ada71c5f1b67f5864a55c0605643e035a72531f
sam-uit <25410291@ms.uit.edu.vn>
2026-01-17 14:46:49 +0800

    Create cacmohinhcongtac-mm.mm

---
+++ content/diagrams/cacmohinhcongtac-mm.mm
@@ -0,0 +1,36 @@
+---
+config:
+  theme: mc
+  themeVariables:
+    primaryColor: '#e1f5fe'
+    primaryTextColor: '#0277bd'
+    primaryBorderColor: '#0288d1'
+    lineColor: '#0288d1'
+    fontFamily: ''
+    fontSize: 16px
+---
+mindmap
+((Các Mô Hình<br>Cộng Tác))
+  (Mô hình Centaur<br>Tri thức)
+    (Môi trường)
+      Văn phòng
+      Nghiên cứu
+      Sáng tạo nội dung
+    (Sự cộng tác)
+      Con người: Khung sườn
+      AI: Viết chi tiết
+      Con người: Tinh chỉnh
+    (Giá trị cốt lõi)
+      Tối ưu hóa<br>tư duy chiến lược
+  (Mô hình Cobot<br>Sản xuất)
+    (Môi trường)
+      Nhà máy
+      Dây chuyền lắp ráp
+      Logistics
+    (Sự cộng tác)
+      Robot làm cạnh người
+      Hỗ trợ việc nặng
+      Đòi hỏi chính xác cao
+    (Giá trị cốt lõi)
+      Tối ưu hóa an toàn
+      Hiệu suất vật lý
