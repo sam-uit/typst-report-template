bfe11a6bda5771fa3cd4de501fcdc97a1e495745
sam-uit <25410291@ms.uit.edu.vn>
2026-01-17 15:39:33 +0800

    Create loiichdinhluong.mm

---
+++ content/diagrams/loiichdinhluong.mm
@@ -0,0 +1,16 @@
+---
+config:
+  theme: base
+  themeVariables:
+    primaryColor: '#e1f5fe'
+    primaryTextColor: '#0277bd'
+    primaryBorderColor: '#0288d1'
+    lineColor: '#0288d1'
+    fontFamily: ''
+    fontSize: 16px
+---
+mindmap
+(**Lợi Ích Định Lượng**)
+  (Tăng Năng Suất)
+  (Tăng Trưởng Kinh Tế)
+  (Tiết Kiệm Chi Phí)
