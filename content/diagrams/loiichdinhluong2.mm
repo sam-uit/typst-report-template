a82541fea3a44d24b62bd2b3e5f93b625327ab8c
sam-uit <25410291@ms.uit.edu.vn>
2026-01-17 20:53:19 +0800

    Update loiichdinhluong2.mm

--- content/diagrams/loiichdinhluong2.mm
+++ content/diagrams/loiichdinhluong2.mm
@@ -1,16 +1,20 @@
 ---
 config:
-  theme: redux-color
+  theme: neo 
   themeVariables:
-    primaryColor: '#e1f5fe'
-    primaryTextColor: '#0277bd'
-    primaryBorderColor: '#0288d1'
-    lineColor: '#0288d1'
     fontFamily: Google Sans
-    fontSize: 24px
+    fontSize: 20px
 ---
 mindmap
 root((fa:fa-chart-bar <br/> **LỢI ÍCH ĐỊNH LƯỢNG**))
   ( **Tăng Năng Suất**<br/> fa:fa-rocket <br/> Tối ưu đầu ra trên mỗi đơn vị đầu vào)
+    (1. Giá Trị)
+      Tự Động Hóa các quy trình lặp lại.
+      Tối Ưu Tài Nguyên & Năng Lượng.
+      Giảm Sai Sót do Con Người.
+    (2. Chuyển Đổi)
+      Công việc chuyên môn sâu.
+      Phân tích, sáng tạo, ra quyết định
+      Giảm áp lực và quá tải công việc
   ( **Tăng Trưởng Kinh Tế**<br/> fa:fa-chart-line <br/> Thúc đẩy GDP và mở rộng thị trường)
   ( **Tiết Kiệm Chi Phí**<br/> fa:fa-piggy-bank <br/> Cắt giảm lãng phí và chi phí vận hành)
