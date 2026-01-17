c94e0f509db02594bec9df84d9ecf2e53bc425b0
sam-uit <25410291@ms.uit.edu.vn>
2026-01-17 14:58:58 +0800

    Create tacdongtuduy.mm

---
+++ content/diagrams/tacdongtuduy.mm
@@ -0,0 +1,22 @@
+---
+config:
+  theme: neo
+  themeVariables:
+    fontFamily: Google Sans
+    fontSize: 16px
+---
+mindmap
+((**Tác Động Đến Tư Duy Quản Trị Và Đào Tạo**))
+  (**Chuyển Đổi Vai Trò**)
+    (Trước Đây)
+      Thực hiện nghiệp vụ
+    (Bây Giờ)
+      Hướng dẫn, Thẩm định quá trình và kết quả
+  (**Giá Trị Nhân Văn**)
+    (Máy Móc)
+      Thực hiện nghiệp vụ
+      Hoàn thành mục tiêu
+    (Con Người)
+      Giá trị cốt lõi
+      Sự thấu cảm
+      Đạo đức nghề nghiệp
