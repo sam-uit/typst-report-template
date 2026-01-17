7325a0fa43649e102091a0b5a1e60e3bcf2c286e
sam-uit <25410291@ms.uit.edu.vn>
2026-01-17 17:42:46 +0800

    Create loiichdinhluong2.mm

---
+++ content/diagrams/loiichdinhluong2.mm
@@ -0,0 +1,16 @@
+---
+config:
+  theme: redux-color
+  themeVariables:
+    primaryColor: '#e1f5fe'
+    primaryTextColor: '#0277bd'
+    primaryBorderColor: '#0288d1'
+    lineColor: '#0288d1'
+    fontFamily: Google Sans
+    fontSize: 16px
+---
+mindmap
+root((fa:fa-chart-bar **LỢI ÍCH <br>ĐỊNH LƯỢNG**))
+  (fa:fa-rocket **Tăng Năng Suất**<br/>Tối ưu đầu ra trên<br/>mỗi đơn vị đầu vào)
+  (fa:fa-chart-line **Tăng Trưởng<br/>Kinh Tế**<br/>Thúc đẩy GDP và<br/>mở rộng thị trường)
+  (fa:fa-piggy-bank **Tiết Kiệm<br/>Chi Phí**<br/>Cắt giảm lãng phí và<br/>chi phí vận hành)
