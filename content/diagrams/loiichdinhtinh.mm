21a3bfe176527ffec69a2d3cbd8b03c0c73747d4
sam-uit <25410291@ms.uit.edu.vn>
2026-01-17 17:02:02 +0800

    Create loiichdinhtinh.mm

---
+++ content/diagrams/loiichdinhtinh.mm
@@ -0,0 +1,17 @@
+---
+config:
+  theme: base
+  themeVariables:
+    primaryColor: '#e1f5fe'
+    primaryTextColor: '#0277bd'
+    primaryBorderColor: '#0288d1'
+    lineColor: '#0288d1'
+    fontFamily: 'Google Sans'
+    fontSize: 16px
+---
+mindmap
+((**Lợi Ích <br>Định Tính**))
+  (Giải Phóng Sức Lao Động Sáng Tạo)
+  (Cá Nhân Hóa Trải Nghiệm Và Tăng Cường Sự Gắn Kết)
+  (Củng Cố Niềm Tin Và Sự An Tâm Trong Đời Sống)
+  (Thay Đổi Văn Hóa Và Mô Hình Vận Hành Tổ Chức)
